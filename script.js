40635068ab49ac45e84557e7a13c840046166631
Challa Usha Rani <ushashravana2006@gmail.com>
2026-07-24 08:34:42 +0530

    Day 7 completed

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const blogs = [
+let blogs = [
     {
         title: "Importance of Reading",
         content: "Reading books improves knowledge, vocabulary, and creativity."
@@ -9,20 +9,48 @@ const blogs = [
     }
 ];
 
-const container = document.getElementById("blogContainer");
+function displayBlogs() {
+    const container = document.getElementById("blogContainer");
+    container.innerHTML = "";
 
-blogs.forEach(blog => {
-    const card = document.createElement("div");
-    card.innerHTML = `
-        <h2>${blog.title}</h2>
-        <p>${blog.content}</p>
-        <hr>
-    `;
-    container.appendChild(card);
-});
+    blogs.forEach((blog, index) => {
+        const card = document.createElement("div");
 
+        card.innerHTML = `
+            <h2>${blog.title}</h2>
+            <p>${blog.content}</p>
+
+            <button onclick="editBlog(${index})">Edit</button>
+            <button onclick="deleteBlog(${index})">Delete</button>
+            <hr>
+        `;
+
+        container.appendChild(card);
+    });
+}
 function submitBlog(event){
     event.preventDefault();
     
-alert("Blog Added Successfully");
+    alert("Blog Added Successfully");
+   }
+
+
+function deleteBlog(index) {
+    blogs.splice(index, 1);
+    alert("Blog deleted successfully!");
+    displayBlogs();
 }
+
+function editBlog(index) {
+    let newTitle = prompt("Enter new blog title:", blogs[index].title);
+    let newContent = prompt("Enter new blog content:", blogs[index].content);
+
+    if (newTitle && newContent) {
+        blogs[index].title = newTitle;
+        blogs[index].content = newContent;
+        alert("Blog updated successfully!");
+        displayBlogs();
+    }
+}
+
+displayBlogs();
